209e2f20a5f35f297f2583685d01a69ded40134b
ZetGar <88043986+ZetGar@users.noreply.github.com>
2021-09-03 01:19:03 +0900

    Update withland.css

--- js/withland.js
+++ js/withland.js
@@ -1,10 +1,40 @@
 $(function(){
   const $slide = $('.mn_slide');
-  const $dogslide = $('.mn_slide li');
+  const $thmbs = $('.mn_pagination li>a');
 
-  const nowIdx=0;
+  let nowIdx=Math.floor(Math.random()*4);
+  let intervalKey;
+  
+  const moveFn=function(){
+    $thmbs.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
 
-  $(window).on('lode', function(){
-    
+    $slide.stop().animate({left: -1200*nowIdx},'easeInOutCubic');
+  }
+
+  $thmbs.on('click', function(evt){
+    evt.preventDefault();
+
+    $(this).parent().addClass('on').siblings().removeClass('on');
+
+    nowIdx = $thmbs.index(this);
+
+    moveFn();
   });
+
+  $(window).on('load',function(){
+    clearInterval(intervalKey);
+
+    moveFn();
+
+    intervalKey=setInterval(()=>{
+      if(nowIdx<4){
+        nowIdx++;
+      }else{
+        nowIdx=0;
+      }
+      moveFn();
+    },2000);
+  });
+
+ 
 });
